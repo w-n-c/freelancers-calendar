3fa6a44b4771bc9e3716840b84e0cfe480f4e47e
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-01 14:17:59 -0400

    stub out calendar components

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
+import Calendar from './Calendar'
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
 		<Header />
+		<Calendar />
       </div>
     );
   }
---
+++ src/App/Calendar/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Monthly from './Monthly'
+import Weekly from './Weekly'
+import Daily from './Daily'
+
+const props = {
+	view: 'monthly'
+}
+
+export const Calendar = () => (
+	(props.view === 'monthly') ? <Monthly />
+		: (props.view === 'weekly') ? <Weekly />
+		: <Daily /> 
+)
