261935d7d013847868c7545d5f41b46d09dfb1f5
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-01 13:46:26 -0400

    restructure create-react-app and add header stub component

---
+++ src/App/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const props = {
+	today: '2019/04/01',
+	year: 2019,
+	month: {
+		title: 'April',
+		number: '4'
+	}
+}
+
+export const Header = () => (
+	<header>
+		<button>Hamburger</button>
+		<button>
+			<a href={`/${props.today}`}>
+				Today
+			</a>
+		</button>
+		<button>
+			<a href="last_month">
+				&lt;
+			</a>
+		</button>
+		<button>
+			<a href="next_month">
+				&gt;
+			</a>
+		</button>
+		<h2>
+			{props.month.title} {props.year}
+		</h2>
+	</header>
+)
