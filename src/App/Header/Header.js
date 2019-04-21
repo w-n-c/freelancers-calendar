0fa188b62b0541508779cea580d2c69c83b47881
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-21 04:23:32 -0400

    update navigation

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -7,15 +7,24 @@ const getMonthName = (month) => {
 }
 
 export const Header = (props) => {
-	const { year, month, day, view } = props
+	const { year, month, day, view, today } = props
 	return (
 		<header>
 			<button>Hamburger</button>
-			<nav>
-				<Link to={`/${view}/${props.today}`}>Today</Link>
-				{/* TODO: need to properly implement month switching*/}
-				<Link to={`/${view}/${year}/${parseInt(month)-1}/${day}`}>&lt;</Link>
-				<Link to={`/${view}/${year}/${parseInt(month)+1}/${day}`}>&gt;</Link>
+			<nav role="navigation">
+				<ul>
+					<li><Link to={`/${view}/${today}`}>Today</Link></li>
+					{/* TODO: need to properly implement month switching*/}
+					<li><Link to={`/${view}/${year}/${parseInt(month)-1}/${day}`}>&lt;</Link></li>
+					<li><Link to={`/${view}/${year}/${parseInt(month)+1}/${day}`}>&gt;</Link></li>
+					<li><a href="#" aria-haspopup="true">View</a>
+						<ul className="dropdown" aria-label="submenu">
+							<li><Link to={`/weekly/${year}/${month}/${day}`}>Weekly</Link></li>
+							<li><Link to={`/monthly/${year}/${month}/${day}`}>Monthly</Link></li>
+							{/*TODO: daily link*/}
+						</ul>
+					</li>
+				</ul>
 			</nav>
 			<h2>
 				{getMonthName(month)} {year}
