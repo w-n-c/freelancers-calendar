865cd8634d7769957613baa2828e54f62b6a9a98
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-27 00:11:59 -0400

    add interval to update today

--- src/App/App.js
+++ src/App/App.js
@@ -8,8 +8,28 @@ import { FAKE_TODOS } from './api'
 export class App extends React.Component {
 	state = {
 		events: FAKE_TODOS,
-		// TODO: today's date should regularly update
 		today: this.getTodaysDate()
+	}
+
+	componentDidMount() {
+		// set a timeout to hit on midnight, updating today's date
+		// at midnight, set an interval for every midnight to update today's date
+		const today = new Date()
+		const tomorrow = new Date(today.getDate()+1).setHours(0,0,0,0)
+		// setHours returns milliseconds so no getTime() needed
+		const timeout = tomorrow - today.getTime()
+		const interval = 60*60*24*1000
+
+		setTimeout(() => {
+			this.setState({today: this.getTodaysDate()})
+			this.intervalId = setInterval(
+				() => this.setState({today: this.getTodaysDate()}),
+				interval)
+		}, timeout)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalId)
 	}
 
 	getTodaysDate() {
@@ -22,7 +42,7 @@ export class App extends React.Component {
 			<div className="App">
 				<EventContext.Provider value={this.state.events}>
 					<Router>
-						<Redirect from="/" to={`monthly/${this.state.today}`} />
+						<Redirect noThrow from="/" to={`monthly/${this.state.today}`} />
 						<Calendar today={this.state.today} path="/:view/:year/:month/:day"/>
 					</Router>
 				</EventContext.Provider>
