140aa8f2101c822db56bba50e3f15865f445b80a
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-29 13:45:09 -0400

    display formatted time in Weekly

--- src/App/Calendar/Weekly/index.js
+++ src/App/Calendar/Weekly/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export const hours = new Array(24).fill().map((_, i) => i)
+export const toTime = (num) => {
+	switch (num.toString().length) {
+		case 1:
+			return `0${num}:00`
+		case 2:
+			return `${num}:00`
+		default:
+			console.log(`Err: incorrect time format, expected 1-2 digit number, got: ${num}`)
+			return 0
+	}
+}
+export const hours = new Array(24).fill().map((_, i) => toTime(i))
 
 export default () => {
 	const days = [1,2,3,4,5,6,7]
@@ -10,5 +21,4 @@ export default () => {
 			{days.map((day, i) => <td key={i}>item</td>)}
 		</tr>
 	)
-
 }
