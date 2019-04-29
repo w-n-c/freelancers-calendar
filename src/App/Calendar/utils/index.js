8e33c5d91ac0868c7807e1354e0ab1663b1e195b
Wil.ly Hellcat <newellwm@gmail.com>
2019-04-29 13:47:33 -0400

    fix bugs in change month util functions

--- src/App/Calendar/utils/index.js
+++ src/App/Calendar/utils/index.js
@@ -1,3 +1,27 @@
+// URL is source of truth for date, and for user readability is 1 indexed
+// Date Obj's month is 0 indexed so lots of off by one
+export const changeMonth = (year, month, day, dir) => {
+	const today = new Date(year, month-1, day)
+	// setting day to zero sets us to last day of prev month
+	// so extra +1 on pev/next
+	switch(dir) {
+		case 'inc':
+			const next = new Date(year, month+1, 0)
+			if (today.getDate() >= next.getDate()) {
+				return next
+			}
+			return new Date(year, month, day)
+		case 'dec':
+			const prev = new Date(year, month-1, 0)
+			if (today.getDate() >= prev.getDate()) {
+				return prev
+			}
+			return new Date(year, month-2, day)
+		default:
+			console.log(`Err: correctDay only accepts 2 directions, given ${dir}`)
+	}
+}
+
 export const incWeek = (year, month, day) => {
 	const d = new Date(year, month-1, parseInt(day)+7)
 	return `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
@@ -8,18 +32,17 @@ export const decWeek = (year, month, day) => {
 	return `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
 }
 
-export const incMonth = (year, month) => {
-	// we 1 index month, Date object 0 indexes, so no addition necessary
-	const d = new Date(year, month)
-	return `${d.getFullYear()}/${d.getMonth()}`
+export const incMonth = (year, month, day) => {
+	const d = changeMonth(year, month, day, 'inc')
+	return `${d.getFullYear()}/${d.getMonth()+1}`
 }
-export const decMonth = (year, month) => {
-	// we 1 index month, Date object 0 indexes, so subtract two
-	const d = new Date(year, month-2)
-	return `${d.getFullYear()}/${d.getMonth()}`
+export const decMonth = (year, month, day) => {
+	const d = changeMonth(year, month, day, 'dec')
+	return `${d.getFullYear()}/${d.getMonth()+1}`
 }
 
 export const getMonthName = (month) => {
+	if (month > 12) return ''
 	// blank entry to index January to 1
 	const months = ['','January','February','March','April','May','June','July','August','September','October','November','December']
 	return months[month]
