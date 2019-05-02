3af2455f45ccf1b55a176fcb95c4520921c77c35
Wil.ly Hellcat <newellwm@gmail.com>
2019-05-02 12:19:41 -0400

    refactor mock api

--- src/App/api.js
+++ src/App/api.js
@@ -1,14 +1,17 @@
-const uuid = require('nanoid')
+const uuid = require('nanoid/async')
 
-const year = 2019
-const month = 4
-export const FAKE_TODOS = []
-	
-for (let day = 1; day <= 30; day++) {
-	const event = {}
-	event.id = uuid()
-	event.start = new Date(year, month, day, Math.random(24), Math.random(60))
-	event.end = new Date(event.start + 1000*60*60) // 1 hr long events
-	event.title = `event no: ${day}`
-	FAKE_TODOS.push(event)
+// will come from server eventually, making async now so that everything is pre-promised
+export const fetchEvents = async () => {
+	const year = 2019
+	const month = 4
+	const fakeEvents = []
+	for (let day = 1; day <= 30; day++) {
+		const event = {}
+		event.id = await uuid()
+		event.start = new Date(year, month, day, Math.random(24), Math.random(60))
+		event.end = new Date(event.start.getTime() + 1000*60*60) // 1 hr long events
+		event.title = `event no: ${day}`
+		fakeEvents.push(event)
+	}
+	return fakeEvents
 }
