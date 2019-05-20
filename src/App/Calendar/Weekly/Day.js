14f78d0ea51e5595dda223bf7e4c9aabebe806b6
Wil.ly Hellcat <newellwm@gmail.com>
2019-05-20 17:08:06 -0400

    set up for weekly to render events

---
+++ src/App/Calendar/Weekly/Day.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default ({day}) =>
+	<td role="cell">item</td>
--- src/App/Calendar/Weekly/index.js
+++ src/App/Calendar/Weekly/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { EventConsumer } from '../../EventContext'
+import Day from './Day'
 
 export const toTime = (num) => {
 	switch (num.toString().length) {
@@ -8,17 +10,34 @@ export const toTime = (num) => {
 			return `${num}:00`
 		default:
 			console.log(`Err: incorrect time format, expected 1-2 digit number, got: ${num}`)
-			return 0
+			return ``
 	}
 }
+
+export const getWeek = (year, month, day) => {
+	// set date to the Sunday (first day) of given week
+	const date = new Date(`${year}/${month}/${day}`)
+	date.setDate(date.getDate() - date.getDay())
+	const week = []
+	while (week.length < 7) {
+		week.push(date.getDate())
+		date.setDate(date.getDate() + 1)
+	}
+	return week
+}
+
 export const hours = new Array(24).fill().map((_, i) => toTime(i))
 
-export default () => {
-	const days = [1,2,3,4,5,6,7]
+export default ({year, month, day}) => {
+	const days = getWeek(year, month, day)
 	return hours.map((hour, i) =>
 		<tr role="row" key={i}>
 			<th role="rowheader" scope="row">{hour}</th>
-			{days.map((day, i) => <td role="cell" key={i}>item</td>)}
+				<EventConsumer>{({ filterTodaysEvents }) =>
+					days.map((day, i) =>
+						<Day key={i} events={filterTodaysEvents(year, month, day)} />
+					)}
+				</EventConsumer>
 		</tr>
 	)
 }
