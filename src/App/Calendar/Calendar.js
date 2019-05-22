3b78d6ce1241badd0868cb8b0a1a04c9eb3b7ded
Wil.ly Hellcat <newellwm@gmail.com>
2019-05-21 23:28:13 -0400

    update comments

--- src/App/Calendar/Calendar.js
+++ src/App/Calendar/Calendar.js
@@ -4,15 +4,20 @@ import Monthly from './Monthly'
 import Weekly from './Weekly'
 import Daily from './Daily'
 
-// changing CSS display on table elements wipes their ARIA role
-// so we reapply those roles to various elements
+// TODO: callback to weekly to add date info to each th
+// TODO: toggle between weekday abbreviations and full name base on window width
+
 export const Calendar = (props) => {
 	const view = props.view
+
+	// changing CSS display on table elements wipes their ARIA role
+	// so we reapply those roles to various elements
 	return [
 		<Header key="1" {...props} />,
 		<table role="table" key="2">
 			<thead>
 				<tr role="row">
+					{view === 'weekly' && <td></td>}
 					<th role="columnheader" scope="col">Su</th>
 					<th role="columnheader" scope="col">Mo</th>
 					<th role="columnheader" scope="col">Tu</th>
@@ -28,6 +33,8 @@ export const Calendar = (props) => {
 				) : view === 'weekly' ? (
 					<Weekly {...props} />
 				) : (
+					// Will likely be a refactor of Weekly view with a 1 day length
+					// same for any 3 or 4 day views like google cal has
 					<Daily />
 				)}
 			</tbody>
