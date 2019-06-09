319b0761ae02be8b1c455c0605c3cde5e4a1b219
Wil.ly Hellcat <newellwm@gmail.com>
2019-06-08 21:12:51 -0400

    move getDaysOfMonth to utils

--- src/App/Calendar/Monthly/index.js
+++ src/App/Calendar/Monthly/index.js
@@ -1,27 +1,8 @@
 import React from 'react'
 import { EventConsumer } from '../../EventContext'
 import Days from './Days'
-import { chunk } from './utils'
+import { chunk, getDaysOfMonth } from './utils'
 
-// TODO: test getDaysOfMonth
-export const getDaysOfMonth = (year, month, day) => {
-	const date = new Date(`${year}/${month}/${day}`)
-	// sets date to the first sunday that should appear on the calendar
-	// will regularly be a day from the previous month
-	date.setDate(1)
-	date.setDate(1 - date.getDay())
-
-	// Monthly view contains 7 days x 5 weeks,
-	// so create an array of objects containing each date.
-	// Later code uses the date to filter which events to add to the object
-	const days = Array(7 * 6).fill()
-	return days.map(() => {
-		// later code assumes this date is set to midnight
-		const today = date.getDate()
-		date.setDate(today + 1)
-		return { date: today } 
-	})
-}
 
 // TODO: test bracket for refactor
 export default ({year, month, day}) =>
--- src/App/Calendar/Monthly/utils/index.js
+++ src/App/Calendar/Monthly/utils/index.js
@@ -1,1 +1,20 @@
 export { chunk } from './chunk'
+
+// TODO: test getDaysOfMonth
+export const getDaysOfMonth = (year, month, day) => {
+	const date = new Date(`${year}/${month}/${day}`)
+	// sets date to the first sunday that should appear on the calendar
+	// will regularly be a day from the previous month
+	date.setDate(1)
+	date.setDate(1 - date.getDay())
+
+	// Monthly view contains 7 days x 6 weeks,
+	// so create an array of objects containing each date.
+	// Later code uses the date to filter which events to add to the object
+	const days = Array(7 * 6).fill()
+	return days.map(() => {
+		const today = date.getDate()
+		date.setDate(today + 1)
+		return { date: today }
+	})
+}
