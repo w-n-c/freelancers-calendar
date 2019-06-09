f149030589bde7ea511012e3ef6b02fee2b16f2f
Wil.ly Hellcat <newellwm@gmail.com>
2019-06-08 21:39:19 -0400

    refactor to class component to manage displaying form

--- src/App/Calendar/Calendar.js
+++ src/App/Calendar/Calendar.js
@@ -8,38 +8,55 @@ import Event from './Event'
 // TODO: callback to weekly to add date info to each th
 // TODO: toggle between weekday abbreviations and full name base on window width
 
-export const Calendar = (props) => {
-	const view = props.view
+export class Calendar extends React.Component {
+	state = {
+		formRendered: false,
+		selectedEvent: {}
+	}
 
-	// changing CSS display on table elements wipes their ARIA role
-	// so we reapply those roles to various elements
-	return [
-		<Header key="1" {...props} />,
-		<table role="table" key="2" className={view}>
-			<thead>
-				<tr role="row">
-					{view === 'weekly' && <td></td>}
-					<th role="columnheader" scope="col">Su</th>
-					<th role="columnheader" scope="col">Mo</th>
-					<th role="columnheader" scope="col">Tu</th>
-					<th role="columnheader" scope="col">We</th>
-					<th role="columnheader" scope="col">Th</th>
-					<th role="columnheader" scope="col">Fr</th>
-					<th role="columnheader" scope="col">Sa</th>
-				</tr>
-			</thead>
-			<tbody>
-				{view === 'monthly' ? (
-					<Monthly {...props}/>
-				) : view === 'weekly' ? (
-					<Weekly {...props} />
-				) : (
-					// Will likely be a refactor of Weekly view with a 1 day length
-					// same for any 3 or 4 day views like google cal has
-					<Daily />
-				)}
-			</tbody>
-		</table>,
-		<Event key="3"/>
-	]
+	handleClick = (event) => {
+		this.setState({
+			formRendered: true,
+			selectedEvent: event
+		})
+	}
+
+	renderForm() {
+		return <Event event={this.state.selectedEvent} />
+	}
+
+	render() {
+		// changing CSS display on table elements wipes their ARIA role
+		// so we reapply those roles to various elements
+		const view = this.props.view
+		return <div>
+			<Header key="1" {...this.props} />
+			<table role="table" key="2" className={view}>
+				<thead>
+					<tr role="row">
+						{view === 'weekly' && <td></td>}
+						<th role="columnheader" scope="col">Su</th>
+						<th role="columnheader" scope="col">Mo</th>
+						<th role="columnheader" scope="col">Tu</th>
+						<th role="columnheader" scope="col">We</th>
+						<th role="columnheader" scope="col">Th</th>
+						<th role="columnheader" scope="col">Fr</th>
+						<th role="columnheader" scope="col">Sa</th>
+					</tr>
+				</thead>
+				<tbody>
+					{view === 'monthly' ? (
+						<Monthly {...this.props} handleClick={this.handleClick}/>
+					) : view === 'weekly' ? (
+						<Weekly {...this.props} />
+					) : (
+						// Will likely be a refactor of Weekly view with a 1 day length
+						// same for any 3 or 4 day views like google cal has
+						<Daily />
+					)}
+				</tbody>
+			</table>
+			{this.state.formRendered ? this.renderForm() : '' }
+		</div>
+	}
 }
