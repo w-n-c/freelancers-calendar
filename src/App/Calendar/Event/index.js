d75f4aed8600efc52240892d1a486f23c56d5090
Wil.ly Hellcat <newellwm@gmail.com>
2019-06-08 21:38:32 -0400

    form receives event as props to allow updating

--- src/App/Calendar/Event/index.js
+++ src/App/Calendar/Event/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import EventForm from './EventForm'
 import { EventConsumer } from '../../EventContext'
+import { toDateString, toTimeString } from '../utils'
 
 export const putEvent = (event, date) => {}
 
@@ -12,4 +13,27 @@ export const newEvent = (event, date) => {}
 // calling with a date opens a menu to create a new event at that date
 // calling with both moves the start time of the event to the new date
 
-export default (props) => <EventForm />
+export default (props) =>
+	<EventConsumer>{({handleCreateEvent, handleUpdateEvent}) => {
+		const event = props.event || {}
+		const date = props.date || {}
+
+		const start = new Date(event.start)
+		const startDate = toDateString(start) || toDateString(date)
+		const startTime = toTimeString(start) || toTimeString(date)
+
+		const end = new Date(event.end)
+		const endDate = toDateString(end) || toDateString(date)
+		const endTime = toTimeString(end)
+
+		const formInput = {
+			eventTitle: event.title,
+			startDate,
+			startTime,
+			endDate,
+			endTime,
+			description: event.description
+		}
+
+		return <EventForm {...formInput} />
+	}}</EventConsumer>
