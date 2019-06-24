e3a1e3c7a907f9cebb0e304558eeddae3385daac
William Newell <newellwm@gmail.com>
2019-06-24 15:30:38 -0400

    Styled events in weekly based on event time
    Events in weekly view are now positioned relative to the the Day/Time table.
    Their top position is offset from the nearest hour to their actual start
    time and their length accurately reflects the length of the event.
    There is no handling of overlapping events and events don't correctly
    run over to the next day.

--- src/App/Calendar/Weekly/Hour.js
+++ src/App/Calendar/Weekly/Hour.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
+const diffInMin = (t1, t2) =>
+	Math.abs(Math.round(
+		(new Date(t1) - new Date(t2))
+		/ 60000
+	))
+
 export default ({events}) =>
 	<td role="cell">
-		{events.map((event, i) => <div key={i}>{event.title}</div>)}
+		{events.map((event, i) => {
+			const style = {
+				position: 'relative',
+				background: 'lightblue',
+				color: 'white'
+			}
+			const minutes = new Date(event.start).getMinutes()
+			const length = diffInMin(event.start, event.end)
+
+			style.top = `calc(${minutes/60}*5vw)`
+			style.height = `calc(${length/60}*5vw`
+
+			return <div style={style} key={i}>
+				{event.title}
+			</div>
+		})}
 	</td>
