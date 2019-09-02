31163e777c923901738707387ba05966c9262d00
William Newell <newellwm@gmail.com>
2019-09-02 17:53:50 -0400

    Minor code cleanup

--- src/App/Calendar/Calendar.js
+++ src/App/Calendar/Calendar.js
@@ -3,14 +3,10 @@ import { pick } from 'lodash/fp'
 import Event from './Event'
 import CalendarHeader from './CalendarHeader'
 
-// TODO: callback to weekly to add date info to each th
-// TODO: toggle between weekday abbreviations and full name base on window width
-
-
 export function Calendar(props) {
 
 	const day = pick(['year', 'month', 'date'], props)
-	const { handleUpdateToday, render, view } = props
+	const { handleUpdateToday, render } = props
 
 	const [formRendered, toggleForm] = useState(false)
 	const [event, updateEvent] = useState({})
@@ -32,9 +28,12 @@ export function Calendar(props) {
 		updateEvent({})
 	}
 
+	// Only the weekly view adds dates to the Calendar's column header
+	const showDates = (props.view === 'weekly')
+
 	return (
 		<main onMouseMove={handleMouseMove} role="grid">
-			<CalendarHeader day={view === 'weekly' ? day : undefined} />
+			<CalendarHeader day={showDates ? day : undefined} />
 			{render(day, handleClick)}
 			{formRendered ?
 				<Event
