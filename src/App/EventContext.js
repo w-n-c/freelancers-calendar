297dcb0eefc5e3da72d6aca1bc1cefa6c1f1cbca
Wil.ly Hellcat <newellwm@gmail.com>
2019-09-04 17:17:45 -0400

    Refactor EventContext into function component

--- src/App/EventContext.js
+++ src/App/EventContext.js
@@ -1,75 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assign } from 'lodash'
 const events = require('./events.json')
 const uuid = require('nanoid')
 
 const EventContext = React.createContext()
 const { Provider, Consumer } = EventContext
 
-class EventProvider extends React.Component {
-	state = {
-		events: [],
+const EventProvider = (props) => {
+	const [state, newState] = useState({
+		events,
 		loading: false,
 		error: ''
-	}
+	})
 
-	componentDidMount() {
-		this.setState({ loading: false, events })
-		//fetchEvents()
-		//	.then(events => this.setState({ loading: false, events }))
-		//	.catch(error => this.setState({ loading: false, error}))
-	}
+	const setState = assign(newState)
 
-	filterTodaysEvents = ({year, month, date}) => {
-		const startDate = new Date(`${year}/${month}/${date}`)
-		const endDate = new Date(startDate); endDate.setHours(24)
+	const filterTodaysEvents = ({year, month, date}) => {
 
-		return this.state.events.filter(event => {
-			const eventStart = new Date(event.start)
-			return eventStart >= startDate && eventStart < endDate
-		})
+		const isInDay = day => date => {
+			const dayStart = new Date(day)
+			const dayEnd = new Date(dayStart); dayEnd.setHours(24)
+			const time = new Date(date)
+
+			return time >= dayStart && time < dayEnd
+		}
+
+		const today = new Date(`${year}/${month}/${date}`).toISOString()
+		const isToday = isInDay(today)
+		const eventIsToday = event => isToday(event.start) || isToday(event.end)
+
+		return state.events.filter(eventIsToday)
 	}
 
 	// TODO: handlers should return promises
-	createEvent = (event) => {
+	const createEvent = (event) => {
 		event.id = uuid()
 		// POST event to server
 		// on success:
-		const events = [...this.state.events, event]
-		this.setState({ events })
+		const events = [...state.events, event]
+		setState({ events: events })
 		return true
 	}
 
-	updateEvent = (update) => {
-		const events = [...this.state.events]
+	const updateEvent = (update) => {
+		const events = [...state.events]
 		const eventIndex = events.findIndex(
 			event => event.id === update.id
 		)
 		events[eventIndex] = update
-		this.setState({ events })
+		setState({ events })
 		return true
 	}
 
-	handleDeleteEvent = (deleted) => {
-		const events = [...this.state.events]
+	const deleteEvent = (deleted) => {
+		const events = [...state.events]
 		const eventIndex = events.findIndex(
 			event => event.id === deleted.id
 		)
 		events.splice(eventIndex, eventIndex+1)
-		this.setState({ events })
+		setState({ events })
 		return true
 	}
 
-	render() {
-		return (
-			<Provider value={{
-				...this.state,
-				filterTodaysEvents: this.filterTodaysEvents,
-				handleCreateEvent: this.createEvent,
-				handleUpdateEvent: this.updateEvent,
-				handleDeleteEvent: this.deleteEvent,
-			}}>{this.props.children}</Provider>
-		)
-	}
+	return (
+		<Provider value={{
+			...state,
+			filterTodaysEvents: filterTodaysEvents,
+			handleCreateEvent: createEvent,
+			handleUpdateEvent: updateEvent,
+			handleDeleteEvent: deleteEvent,
+		}}>{props.children}</Provider>
+	)
 }
 
 
