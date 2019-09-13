003c32ad467ebb602da3e5d288f78f0326ad85f5
Wil.ly Hellcat <newellwm@gmail.com>
2019-09-13 16:08:51 -0400

    Minor code cleanup

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,60 +1,53 @@
 import React from 'react'
 import {Link} from '@reach/router'
+import { pick } from 'lodash/fp'
 import {incWeek, decWeek, incMonth, decMonth, getMonthName} from './utils'
 
 export default (props) => {
-	const { year, month, date, view, today } = props
+	const { view, today } = props
+	const day = pick(['year', 'month', 'date'], props)
 
-	// TODO: write ALL the tests (including util fns)
-	const getNextMonthUrl = () => {
-		return `/${view}/${incMonth(year, month, date)}`
-	}
-	const getPrevMonthUrl = () => {
-		return `/${view}/${decMonth(year, month, date)}`
-	}
-	const getNextWeekUrl = () => {
-		return `/${view}/${incWeek(year, month, date)}`
-	}
-	const getPrevWeekUrl = () => {
-		return `/${view}/${decWeek(year, month, date)}`
-	}
+	const dateString = `${day.year}/${day.month}/${day.date}`
+	const nextMonthUrl = `/monthly/${incMonth(day)}`
+	const prevMonthUrl = `/monthly/${decMonth(day)}`
+	const nextWeekUrl  = `/weekly/${incWeek(day)}`
+	const prevWeekUrl  = `/weekly/${decWeek(day)}`
 
 	return (
 		<header className="site-header">
 			<nav role="navigation" aria-labelledby="calendar-date">
 				<ul>
-					<h1 id="calendar-date">{getMonthName(month)} {year}</h1>
+					<h1 id="calendar-date">{getMonthName(day.month)} {day.year}</h1>
 					<li>
 						<Link title="Return to Today" to={`/${view}/${today}`}>Today</Link>
 					</li>
 					<li>
 						{props.view === 'monthly' &&
-							<Link title="Previous Month" to={getPrevMonthUrl()}>&lt;</Link>
+							<Link title="Previous Month" to={prevMonthUrl}>&lt;</Link>
 						}{props.view === 'weekly' &&
-							<Link title="Previous Week" to={getPrevWeekUrl()}>&lt;</Link>
+							<Link title="Previous Week" to={prevWeekUrl}>&lt;</Link>
 						}
 					</li>
 					<li>
 						{props.view === 'monthly' &&
-							<Link title="Next Month" to={getNextMonthUrl()}>&gt;</Link>
+							<Link title="Next Month" to={nextMonthUrl}>&gt;</Link>
 						}{props.view === 'weekly' &&
-							<Link title="Next Week" to={getNextWeekUrl()}>&gt;</Link>
+							<Link title="Next Week" to={nextWeekUrl}>&gt;</Link>
 						}
 					</li>
 					<li>
 						<button aria-haspopup="true">View</button>
 						<ul className="dropdown" aria-label="submenu">
 							<li>
-								<Link title="Weekly View" to={`/weekly/${year}/${month}/${date}`}>
+								<Link title="Weekly View" to={`/weekly/${dateString}`}>
 									Weekly
 								</Link>
 							</li>
 							<li>
-								<Link title="Monthly View" to={`/monthly/${year}/${month}/${date}`}>
+								<Link title="Monthly View" to={`/monthly/${dateString}`}>
 									Monthly
 								</Link>
 							</li>
-							{/*TODO: daily link*/}
 						</ul>
 					</li>
 				</ul>
--- src/App/Header/utils/index.js
+++ src/App/Header/utils/index.js
@@ -6,30 +6,24 @@ export const getMonthName = (month) => {
 	return months[index]
 }
 
-export const incWeek = (year, month, day) => {
-	return toDateString(new Date(year, month-1, parseInt(day)+7))
-}
-
-export const decWeek = (year, month, day) => {
-	return toDateString(new Date(year, month-1, day-7))
-}
-
-export const incMonth = (year, month, day) => {
-	return toDateString(changeMonth(year, month, day, 'inc'))
-}
-
-export const decMonth = (year, month, day) => {
-	return toDateString(changeMonth(year, month, day, 'dec'))
-}
-
-// This code is impossible to reason about and shouldn't be aware of the 1 indexing
-// of the Date source. TODO: Refactor
-
 // Source of truth for date is user visible and therefore month is 1 indexed
 // JS Date's month is 0 indexed so lots of off-by-ones in the following code
 
-export const changeMonth = (year, month, day, dir) => {
-	const today = new Date(year, month-1, day)
+export const incWeek = ({year, month, date}) =>
+	toDateString(new Date(year, month-1, parseInt(date)+7))
+export const decWeek = ({year, month, date}) =>
+	toDateString(new Date(year, month-1, date-7))
+
+export const incMonth = (day) => toDateString(changeMonth(day, 'inc'))
+export const decMonth = (day) => toDateString(changeMonth(day, 'dec'))
+
+// Change month exists to handle issues caused by months containing a varying number of days.
+
+// This code is difficult to reason about and shouldn't be aware of the 1 indexing
+// of the Date source. TODO: Refactor
+
+export const changeMonth = ({year, month, date}, dir) => {
+	const today = new Date(year, month-1, date)
 	// setting day to zero sets us to last day of prev month
 	// so extra +1 on pev/next
 	switch(dir) {
@@ -38,13 +32,13 @@ export const changeMonth = (year, month, day, dir) => {
 			if (today.getDate() >= next.getDate()) {
 				return next
 			}
-			return new Date(year, month, day)
+			return new Date(year, month, date)
 		case 'dec':
 			const prev = new Date(year, month-1, 0)
 			if (today.getDate() >= prev.getDate()) {
 				return prev
 			}
-			return new Date(year, month-2, day)
+			return new Date(year, month-2, date)
 		default:
 			console.log(`Err: correctDay only accepts 2 directions, given ${dir}`)
 	}
