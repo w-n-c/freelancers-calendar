4f680ecf807b9c8db2a0c0b60e4ec7164f0a4c45
Wil.ly Hellcat <newellwm@gmail.com>
2019-09-27 19:34:10 -0400

    Rename variables in getDaysOfMonth

--- src/App/Calendar/Monthly/utils/index.js
+++ src/App/Calendar/Monthly/utils/index.js
@@ -10,15 +10,15 @@ export const getDaysOfMonth = ({year, month, date}) => {
 	// Later code uses the date to filter which events to add to the object
 	const days = Array(7 * 6).fill()
 	return days.map(() => {
-		const today = day.getDate()
-		const thisMonth = day.getMonth() + 1
-		const thisYear = day.getFullYear()
+		const date = day.getDate()
+		const month = day.getMonth() + 1
+		const year = day.getFullYear()
 		// SIDE EFFECT
-		day.setDate(today + 1)
+		day.setDate(date + 1)
 		return {
-			year: thisYear,
-			month: thisMonth,
-			date: today
+			year,
+			month,
+			date
 		}
 	})
 }
