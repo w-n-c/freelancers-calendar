60710edf4ca76a216f672e9f43cadc24058b882f
Wil.ly Hellcat <newellwm@gmail.com>
2019-10-02 17:33:43 -0400

    Event now implements useEffect to handle deletion.
    The deletion click handler now updates an Event state that the useEffect
    component checks upon unmounting. While updating Event with useEffect a
    couple small clean-up refactors were also performed.

--- src/App/Calendar/Event/index.js
+++ src/App/Calendar/Event/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import EventForm from './EventForm'
 import EventContext from '../../EventContext'
 import { isoDateToCalStrings } from '../utils'
@@ -8,34 +8,33 @@ const newEvent = (year, month, date) => ({
 	end: new Date(`${year}/${month}/${date}`).toISOString()
 })
 
+
 export default (props) => {
-	const {getEvent, handleCreateEvent, handleUpdateEvent, handleDeleteEvent} = useContext(EventContext)
+	const { getEvent, handleCreateEvent, handleUpdateEvent, handleDeleteEvent } = useContext(EventContext)
 	const { route, year, month, date, id } = props
 
-	const handleDelete = (id) => {
-		// temporary workaround to the fact the component is deleting itself
-		// gives time for the async navigation/rerender to occur
-		window.setTimeout(handleDeleteEvent, 100, id)
-		props.navigate(`/${route}/${year}/${month}/${date}`)
-	}
+	const [eventToDelete, setDeletion] = useState(false)
+	useEffect(() => () => {if (eventToDelete) handleDeleteEvent(eventToDelete)})
+
+	const closeForm = () => props.navigate(`/${route}/${year}/${month}/${date}`)
+
 	const handleSubmit = (event) => {
 		const success = event.id ? handleUpdateEvent(event) : handleCreateEvent(event)
 		// Need to create an error handling system
 		// placeholders as reminder
 		if (success) {
-			props.navigate(`/${route}/${year}/${month}/${date}`)
+			closeForm()
 		}
 	}
 
-	const handleClose = () => props.navigate(`/${route}/${year}/${month}/${date}`)
-
-	let event = {}
-	if (id === 'new') {
-		event = newEvent(year, month, date)
-	} else {
-		event = getEvent(id)
+	const handleDelete = (id) => {
+		setDeletion(id)
+		closeForm()
 	}
 
+	const handleClose = closeForm
+
+	const event = (id === 'new') ? newEvent(year, month, date) : getEvent(id)
 	const [startDate, startTime] = isoDateToCalStrings(event.start)
 	const [endDate, endTime] = isoDateToCalStrings(event.end)
 
