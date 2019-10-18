95dce95529e17587f3f3b047a7ef157e6040fe2f
Wil.ly Hellcat <newellwm@gmail.com>
2019-10-18 18:32:40 -0400

    Partially refactor Header

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import {Link} from '@reach/router'
-import { pick } from 'lodash/fp'
+import { Link } from '@reach/router'
+import { pick, curry } from 'lodash/fp'
 import {incWeek, decWeek, incMonth, decMonth, getMonthName} from './utils'
 
+const _url = (eventUrl, view, date) => `/${view}/${date}/${eventUrl}`
+export const url = curry(_url)
+
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+const viewToNoun = (s) => s.charAt(0).toUpperCase() + s.slice(1,-2);
+
 export default (props) => {
-	const { view, today } = props
+	const { view, today, '*': eventPath, location: { search }} = props
 	const day = pick(['year', 'month', 'date'], props)
 
-	const dateString = `${day.year}/${day.month}/${day.date}`
-	const nextMonthUrl = `/monthly/${incMonth(day)}/${props['*']}`
-	const prevMonthUrl = `/monthly/${decMonth(day)}/${props['*']}`
-	const nextWeekUrl  = `/weekly/${incWeek(day)}/${props['*']}`
-	const prevWeekUrl  = `/weekly/${decWeek(day)}/${props['*']}`
+	const eventInfo = `${eventPath}${search}`
+	const eventUrl = url(eventInfo)
 
+	const viewUrl = eventUrl(view)
+	const weeklyUrl = eventUrl('weekly')
+	const monthlyUrl = eventUrl('monthly')
+
+	const nextMonthUrl = monthlyUrl(incMonth(day))
+	const prevMonthUrl = monthlyUrl(decMonth(day))
+	const nextWeekUrl  = weeklyUrl(incWeek(day))
+	const prevWeekUrl  = weeklyUrl(decWeek(day))
+	const todaysUrl    = viewUrl(today)
+	const toWeeklyUrl  = weeklyUrl(today)
+	const toMonthlyUrl = monthlyUrl(today)
+
+	// had the idea to remove all the ternaries below but need to consider how to deal with the urls also changing
+	// probably need to make another component and pase the title and url as props
+	// will do next commit
+	const title = {
+		noun: viewToNoun(view),
+		adjective: capitalize(view)
+	}
+
+	console.log(title)
 	return (
 		<header className="site-header">
 			<nav role="navigation" aria-labelledby="calendar-date">
 				<ul>
 					<h1 id="calendar-date">{getMonthName(day.month)} {day.year}</h1>
 					<li>
-						<Link title="Return to Today" to={`/${view}/${today}/${props['*']}${props.location.search}`}>Today</Link>
+						<Link title="Return to Today" to={todaysUrl}>Today</Link>
 					</li>
 					<li>
 						{props.view === 'monthly' &&
@@ -39,12 +63,12 @@ export default (props) => {
 						<button aria-haspopup="true">View</button>
 						<ul className="dropdown" aria-label="submenu">
 							<li>
-								<Link title="Weekly View" to={`/weekly/${dateString}/${props['*']}${props.location.search}`}>
+								<Link title="Weekly View" to={toWeeklyUrl}>
 									Weekly
 								</Link>
 							</li>
 							<li>
-								<Link title="Monthly View" to={`/monthly/${dateString}/${props['*']}${props.location.search}`}>
+								<Link title="Monthly View" to={toMonthlyUrl}>
 									Monthly
 								</Link>
 							</li>
