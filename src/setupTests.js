f17a2c3563cb83aeec08c1ea5c2d18c7ad4ea505
Wil.ly Hellcat <newellwm@gmail.com>
2019-10-17 20:15:50 -0400

    Add enzyme globals to tests and eslint

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
-import { configure } from 'enzyme'
+import { configure, shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter()})
+global.shallow = shallow
+global.render = render
+global.mount = mount
