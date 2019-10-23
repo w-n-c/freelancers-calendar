22f315d4b939a5fa57c292eaffb0be098377ef87
Wil.ly Hellcat <newellwm@gmail.com>
2019-10-23 17:19:47 -0400

    padIfLenOne always returns a string

--- src/App/Calendar/utils/formatTime.js
+++ src/App/Calendar/utils/formatTime.js
@@ -1,6 +1,6 @@
 export const zeroPad = string => `0${string}`
 
-export const padIfLenOne = string => string.length === 1 ? zeroPad(string) : string
+export const padIfLenOne = string => string.toString().length === 1 ? zeroPad(string) : `${string}`
 
 export const formatTime = (time) => {
 	const [hours, minutes] = time.split(':')
