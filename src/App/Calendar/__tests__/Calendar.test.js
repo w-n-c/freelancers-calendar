68efcce35cbd1b92f2d09435269d7dd139634805
Wil.ly Hellcat <newellwm@gmail.com>
2020-01-02 19:08:52 -0500

    Add test for handleUpdateToday call on mouseover

--- src/App/Calendar/__tests__/Calendar.test.js
+++ src/App/Calendar/__tests__/Calendar.test.js
@@ -9,9 +9,25 @@ describe('<Calendar />', () => {
 		getUser: jest.fn(),
 		checkEvents: jest.fn()
 	}))
+
 	it('renders without crashing', () => {
 		const div = document.createElement('div')
 		ReactDOM.render(<Calendar />, div)
 		ReactDOM.unmountComponentAtNode(div)
+	})
+
+	it('calls handleUpdateToday on mouseover', () => {
+		const onMouseMove = jest.fn()
+		const preventDefault = jest.fn()
+
+		const wrapper = shallow(
+			<Calendar handleUpdateToday={onMouseMove} />
+		).find('main').shallow()
+
+		expect(preventDefault.mock.calls.length).toBe(0)
+		expect(onMouseMove.mock.calls.length).toBe(0)
+		wrapper.simulate('mouseMove', { preventDefault })
+		expect(onMouseMove.mock.calls.length).toBe(1)
+		expect(preventDefault.mock.calls.length).toBe(1)
 	})
 })
