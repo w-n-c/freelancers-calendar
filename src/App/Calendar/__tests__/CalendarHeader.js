57452e37efef6de3e3afd32003e73d807a870a38
Wil.ly Hellcat <newellwm@gmail.com>
2020-01-17 10:35:57 -0500

    Add unit tests

---
+++ src/App/Calendar/__tests__/CalendarHeader.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CalendarHeader, { weekdayAbbr } from '../CalendarHeader'
+import { getWeek } from '../Weekly/utils'
+
+describe('<CalendarHeader />', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div')
+		ReactDOM.render(<CalendarHeader />, div)
+		ReactDOM.unmountComponentAtNode(div)
+	})
+
+	describe('given different views', () => {
+		const date = { year: 2019, date: 20, month: 7 }
+		const monthly = shallow(<CalendarHeader {...date} route={'monthly'} />)
+		const weekly = shallow(<CalendarHeader {...date} route={'weekly'} />)
+		it('displays the day of the week for both views', () => {
+			monthly.find('SectionHeader').forEach((node, i) => {
+				expect(node.children().first().text()).toEqual(weekdayAbbr[i])
+			})
+			weekly.find('SectionHeader').forEach((node, i) => {
+				expect(node.children().first().text()).toEqual(weekdayAbbr[i])
+			})
+		})
+
+		it('displays the day of the month only for weekly view', () => {
+			const week = getWeek(date)
+			weekly.find('SectionHeader').forEach((node, i) => {
+				expect(node.children().last().text()).toEqual(`${week[i].date}`)
+			})
+			monthly.find('SectionHeader').forEach((node, i) => {
+				expect(node.children().last().text()).toEqual(``)
+			})
+		})
+
+		it('adds padding elements only for weekly view', () => {
+			expect(monthly.find('span')).toHaveLength(0)
+			expect(weekly.find('span')).toHaveLength(2)
+			expect(weekly.find('.header-group').children().first().type()).toEqual('span')
+			expect(weekly.find('.header-group').children().last().type()).toEqual('span')
+		})
+
+	})
+})
