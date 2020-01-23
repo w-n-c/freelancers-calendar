20c3db7f1d932197acd9d3a331dfd6879f00b2dd
Wil.ly Hellcat <newellwm@gmail.com>
2020-01-23 16:36:48 -0500

    Add tests for click handlers in Hour.js

--- src/App/Calendar/Weekly/__tests__/Hour.js
+++ src/App/Calendar/Weekly/__tests__/Hour.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Hour, { getTop, getBottom, offset } from '../Hour'
+import { newEventQuery } from '../../utils'
 describe('<Hour />', () => {
+	afterEach(jest.clearAllMocks)
+	const now = (minutes) => new Date(2020, 0, 23, 16, minutes)
 	const props = {
-		events: [],
+		events: [
+			{ start: now(0), end: now(90), id: 'these' },
+			{ start: now(20), end: now(40), id: 'are' },
+			{ start: now(1000), end: now(1001), id: 'arbitrary' },
+			{ start: now(1000), end: now(1001) } // what happens if missing id?
+		],
 		now: new Date(),
-		navLink: jest.fn()
+		navLink: jest.fn(val => val)
 	}
+
 
 	it('renders without crashing', () => {
 		const div = document.createElement('div')
 		ReactDOM.render(<Hour {...props} />, div)
 		ReactDOM.unmountComponentAtNode(div)
+	})
+
+	it('article calls navLink with a new event on click', () => {
+		const hour = shallow(<Hour {...props} />)
+		hour.simulate('click')
+		expect(props.navLink.mock.calls.length).toBe(1)
+		expect(props.navLink.mock.results[0].value).toEqual(newEventQuery(props.now))
+	})
+
+	it('section calls navLink with event id on click', () => {
+		const hour = shallow(<Hour {...props} />)
+		hour.find('section').forEach((node, index) => {
+			node.simulate('click', { stopPropagation: () => {} })
+			expect(props.navLink.mock.calls.length).toBe(index+1)
+			expect(props.navLink.mock.results[index].value).toEqual(props.events[index].id)
+		})
 	})
 
 	describe('utility functions', () => {
