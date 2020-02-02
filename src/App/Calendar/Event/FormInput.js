69dfba57ce5615e9d3e71638f399678308baa989
Wil.ly Hellcat <newellwm@gmail.com>
2020-02-02 16:06:42 -0500

    Fix header styling and clean up FormInput
    Header styling did not display correctly on weekly view
    because a few styles were applied incorrectly.
    
    FormInput now creates an destructures an object with prop variables for
    a cleaner code list of attributes.

--- src/App/Calendar/Event/FormInput.js
+++ src/App/Calendar/Event/FormInput.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { camelCase, kebabCase } from 'lodash/fp'
 
 const FormInput = (props) => {
-	const { label, wrapperClass, value, handler, autoFocus } = props
+	const { label, wrapperClass, value, handler, autoFocus, type } = props
 	const name = camelCase(label)
 	const id = kebabCase(label)
 	return (
 		<div className={wrapperClass || id}>
-			<label htmlFor={id}>{label}</label>
-			<input
-				autoFocus={autoFocus}
-				name={name}
-				type="text"
-				id={id}
-				onChange={handler}
-				value={value}
-			/>
+			<label>{label}
+				<input {...{
+					autoFocus,
+					name,
+					value,
+					type,
+					onChange: handler
+					}}/>
+			</label>
 		</div>
 	)
 }
