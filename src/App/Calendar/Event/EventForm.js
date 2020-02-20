2ee0e2a64d93c796c12af1544ef3a3e110b84f0b
Wil.ly Hellcat <newellwm@gmail.com>
2020-02-19 20:05:42 -0500

    Event form is now modal
    The event form styling was inconsistent across browsers and did not
    resize well. While fixing these issues, I decided to make it a screen
    centered modal. I do not actually like modals, but for now this seemed
    like the cleanest solution. Eventually it should appear in a minimizable
    box in the bottom right so that the calendar is accessible and multiple
    events can be open for editing at a time.

--- src/App/Calendar/Event/EventForm.js
+++ src/App/Calendar/Event/EventForm.js
@@ -7,11 +7,13 @@ const timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
 
 yup.addMethod(yup.date, 'isChronological', function(){})
 const EventSchema = yup.object({
+	// eslint-disable-next-line
 	title: yup.string().required('title is required').max(200, 'Title cannot exceed ${max} characters in length'),
 	startDate: yup.date().required(),
 	endDate: yup.date().required(),
 	startTime: yup.string().matches(timeRegex, 'must be a valid time'),
 	endTime: yup.string().matches(timeRegex, 'must be a valid time'),
+	// eslint-disable-next-line
 	description: yup.string().notRequired().max(1000, 'Description cannot exceed ${max} characters in length'),
 }).test(
 	'eventHasLength',
@@ -41,82 +43,96 @@ const EventForm = (props) => {
 	}
 	return (
 		<section className="event-menu">
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<legend className="aria-only">Event Information</legend>
-				<label><span className="aria-only">Event Title</span>
-					<input
-						placeholder="Event Title"
-						defaultValue={props.title}
-						name="title"
-						type="text"
-						ref={register()}
-						autoFocus={true}
-					/>
-				{errors.title && <span className="error">{errors.title.message}</span>}
-				</label>
-				<fieldset className="event-start">
-					<legend>Start</legend>
-					<label className="event-date"><span className="aria-only">Start Date</span>
+			<div>
+				<button className="close" aria-label="close" onClick={props.handleClose}><svg
+						viewBox="0 0 12 12"
+						version="1.1" xmlns="http://www.w3.org/2000/svg"
+					>
+						<line x1="1" y1="11" 
+									x2="11" y2="1" 
+									stroke="black" 
+									strokeWidth="2"/>
+						<line x1="1" y1="1" 
+									x2="11" y2="11" 
+									stroke="black" 
+									strokeWidth="2"/>
+						</svg></button>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<legend className="aria-only">Event Information</legend>
+					<label className="event-title"><span className="aria-only">Event Title</span>
 						<input
-							defaultValue={props.startDate}
-							type="date"
-							name="startDate"
+							placeholder="Event Title"
+							defaultValue={props.title}
+							name="title"
+							type="text"
 							ref={register()}
+							autoFocus={true}
 						/>
+					{errors.title && <span className="error">{errors.title.message}</span>}
 					</label>
-					<label className="event-time"><span className="aria-only">Start Time</span>
-						<input
-							defaultValue={props.startTime}
-							type="time"
-							name="startTime"
-							ref={register()}
-						/>
-					</label>
-					{errors.startDate && <span className="error">must have a valid date</span>}
-					{errors.startTime && <span className="error">{errors.startTime.message}</span>}
-				</fieldset>
-				<fieldset className="event-end">
-					<legend>End</legend>
-					<label className="end-date"><span className="aria-only">End Date</span>
-						<input
-							defaultValue={props.endDate}
-							type="date"
-							name="endDate"
-							ref={register()}
-						/>
-					</label>
-					<label className="end-time"><span className="aria-only">End Time</span>
-						<input
-							defaultValue={props.endTime}
-							type="time"
-							name="endTime"
-							ref={register()}
-						/>
-					</label>
-					{errors.endDate && <span className="error">must have a valid date</span>}
-					{errors.endTime && <span className="error">{errors.endTime.message}</span>}
-				</fieldset>
-				<div className="error">
-					{errors.undefined && <span>{errors.undefined.message}</span>}
-				</div>
-				<div className="event-description">
-					<label><span className="aria-only">Description</span>
-						<textarea
-							placeholder="Description..."
-							defaultValue={props.description}
-							name="description"
-							spellCheck="true"
-							ref={register()}
-						></textarea>
-						{errors.description && <span className="error">{errors.description.message}</span>}
-					</label>
-				</div>
-				<input type="submit" value="Save" />
-				<input type="submit" value="Delete" onClick={handleDelete} />
-			</form>
-			<button className="close" aria-label="close" onClick={props.handleClose}>
-				X
-			</button>
+					<fieldset className="event-start">
+						<legend>Start</legend>
+						<label className="event-date"><span className="aria-only">Start Date</span>
+							<input
+								defaultValue={props.startDate}
+								type="date"
+								name="startDate"
+								ref={register()}
+							/>
+						</label>
+						<label className="event-time"><span className="aria-only">Start Time</span>
+							<input
+								defaultValue={props.startTime}
+								type="time"
+								name="startTime"
+								ref={register()}
+							/>
+						</label>
+						{errors.startDate && <span className="error">must have a valid date</span>}
+						{errors.startTime && <span className="error">{errors.startTime.message}</span>}
+					</fieldset>
+					<fieldset className="event-end">
+						<legend>End</legend>
+						<label className="end-date"><span className="aria-only">End Date</span>
+							<input
+								defaultValue={props.endDate}
+								type="date"
+								name="endDate"
+								ref={register()}
+							/>
+						</label>
+						<label className="end-time"><span className="aria-only">End Time</span>
+							<input
+								defaultValue={props.endTime}
+								type="time"
+								name="endTime"
+								ref={register()}
+							/>
+						</label>
+						{errors.endDate && <span className="error">must have a valid date</span>}
+						{errors.endTime && <span className="error">{errors.endTime.message}</span>}
+					</fieldset>
+					<div className="error">
+						{errors.undefined && <span>{errors.undefined.message}</span>}
+					</div>
+					<div className="event-description">
+						<label><span className="aria-only">Description</span>
+							<textarea
+								placeholder="Description..."
+								defaultValue={props.description}
+								name="description"
+								spellCheck="true"
+								ref={register()}
+							></textarea>
+							{errors.description && <span className="error">{errors.description.message}</span>}
+						</label>
+					</div>
+					<div className="form-submit">
+						<input className="save" type="submit" value="Save" />
+						<input className="delete" type="submit" value="Delete" onClick={handleDelete} />
+					</div>
+				</form>
+			</div>
 		</section>
 	)
 }
